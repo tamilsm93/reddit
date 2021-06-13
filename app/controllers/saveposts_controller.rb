e0e7c80dcac4f241075ad8bf1ab3e85715445c9a
sm tamizhazhagan <tamilcrea@gmail.com>
2021-06-13 08:27:04 +0530

    savepost selected data

--- app/controllers/saveposts_controller.rb
+++ app/controllers/saveposts_controller.rb
@@ -1,16 +1,13 @@
 class SavepostsController < ApplicationController
 
 	def index
-		@save_posts = SavePost.all.reverse_order	
+	  @save_posts = SavePost.all.pluck(:title, :description)		
 	end
  
 
 	def create
-	  comment  = Comment.find_by(id: params[:saveposts][:id])
-	  @save_post = SavePost.create(title: comment.title,  description: comment.description) 
-	  
-
-	  
+	  comment    = Comment.find_by(id: params[:saveposts][:id])
+	  @save_post = SavePost.create(title: comment.title,  description: comment.description)   
 	end
 
 end
